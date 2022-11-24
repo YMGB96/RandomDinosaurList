c3f71f9562ba9c642c29b2de0c7fc88c4d5ad35f
YMGB96 <yannick.brands@gmx.net>
2022-11-24 13:47:00 +0100

    Initial Commit

---
+++ FetchGerosRandomJsonURL/FetchGerosRandomJsonURLApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchGerosRandomJsonURLApp.swift
+//  FetchGerosRandomJsonURL
+//
+//  Created by Yannick Brands on 24.11.22.
+//
+
+import SwiftUI
+
+@main
+struct FetchGerosRandomJsonURLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

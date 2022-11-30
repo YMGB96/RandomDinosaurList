4f717d4ea7408b744d76f465f3b1cb5b94605c36
YMGB96 <yannick.brands@gmx.net>
2022-11-30 09:28:18 +0100

    Code slightly(!) better, adding multiple Lists to view

--- FetchGerosRandomJsonURL/ContentView.swift
+++ FetchGerosRandomJsonURL/ContentView.swift
@@ -9,11 +9,29 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var fetchedDataIsShown = false
+    @State var fetchedImage = "leaf.fill"
+    @State var listCount = 0
     var body: some View {
-        FetchedDataView()
+        NavigationView {
+            List {
+                ForEach(0..<listCount, id: \.self) {_ in
+                    FetchedDataView()
+//                if (fetchedDataIsShown == true) {
+//                    FetchedDataView()
+                }
+            }
+            .toolbar {
+                Button{
+//                    fetchedDataIsShown.toggle()
+                    listCount += 1
+                } label: {
+                    Text("Add")
+                }
+            }
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()

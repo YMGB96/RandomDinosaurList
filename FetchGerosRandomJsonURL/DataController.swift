7dd6b07755d3687c4686db1498412398a642c447
YMGB96 <yannick.brands@gmx.net>
2022-12-08 08:03:21 +0100

    Starting up

---
+++ FetchGerosRandomJsonURL/DataController.swift
@@ -0,0 +1,20 @@
+//
+//  DataController.swift
+//  FetchGerosRandomJsonURL
+//
+//  Created by Yannick Brands on 07.12.22.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "FetchedDinoList")
+    
+    
+//    init() {
+//        container.loadPersistentStores { description, error in
+//            if let
+//        }
+    }
+}
